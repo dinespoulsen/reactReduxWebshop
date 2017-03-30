b5dabb3d3e0f3c1641ae761b2d286995cdbb0fb0
Dines Poulsen <dinespoulsen84@gmail.com>
2017-03-30 07:32:41 +0100

    Added a test for prop id for a shoe

--- test/Shoe-test.js
+++ test/Shoe-test.js
@@ -14,6 +14,11 @@ describe("<Shoe />", () => {
     expect(wrapper.props().name).to.equal("shoeName");
   });
 
+  it("should take an Id props", () => {
+    const wrapper = mount(<Shoe id="1"/>);
+    expect(wrapper.props().id).to.equal("1");
+  });
+
   it("should display the price when passed as a prop", () => {
     const wrapper = mount(<Shoe price={10}/>);
     expect(wrapper.find("div").childAt(1).text()).to.equal("Price: 10");
