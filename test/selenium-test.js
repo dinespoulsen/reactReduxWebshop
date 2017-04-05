55f8397364c262f57f05a3a7de5307be5c37a53a
Tacal <TACALTD@legal.regn.net>
2017-04-05 08:02:46 +0100

    Sets up webdriver and writes a test. All ready to go

---
+++ test/selenium-test.js
@@ -0,0 +1,18 @@
+
+import { expect } from 'chai';
+
+let webdriver = require('selenium-webdriver');
+let By = webdriver.By;
+let until = webdriver.until;
+
+let driver = new webdriver.Builder()
+  .forBrowser('firefox')
+  .build();
+
+describe('On page load', function() {
+    it('it should run a test just to see if webdriver works correctly', () => {
+        driver.get('http:localhost:3000')
+        let element = driver.findElement(By.id("7"))
+        expect(element).to.exist;
+    });
+});
